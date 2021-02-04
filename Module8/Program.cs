caa341a33e7adf288791d8939f5839c9eec8ce9f
Alexander <a13x.2002@gmail.com>
2021-02-04 13:52:08 -0700

    This is my module8

---
+++ Module8/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Module8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            HashSet<string> myHashSet = new HashSet<string>();
+
+            bool didItAddTheValue = false;
+
+            didItAddTheValue = myHashSet.Add("first string");
+
+
+            myHashSet.Add("First string");
+            myHashSet.Add("Second string");
+            myHashSet.Add("Second string");
+
+                foreach (var item in myHashSet)
+            {
+                Console.WriteLine(item);
+            }
+
+            Console.WriteLine("the set has this many items");
+        }
+    }
+}
+
+public class MyClass
+{
+
+}
